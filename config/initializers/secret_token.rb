a09a8ef5ff17ac1a53e11ecf5ca50905e4918935
weyewe <w.yunnal@gmail.com>
2014-03-01 13:08:41 +0700

    setup user and role

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brandedbar::Application.config.secret_key_base = '35d5b52248e87a8387856c37fbee9db9f1577fd06219888b23f661590ec048b3679f918bd9012de6a81ccf1e85491fa2656eab435d5abf598332c2a19028269d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Brandedbar::Application.config.session_store :cookie_store, key: '_brandedbar_session'
